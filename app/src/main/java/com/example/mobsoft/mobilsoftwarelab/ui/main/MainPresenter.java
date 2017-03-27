bf2f6a0e0f69306b0562f15081bf5a6bad37fde5
David Kiss <kiss.david@simonyi.bme.hu>
2017-03-27 10:09:10 +0200

    Naive MVP implementation

---
+++ app/src/main/java/com/example/mobsoft/mobilsoftwarelab/ui/main/MainPresenter.java
@@ -0,0 +1,32 @@
+package com.example.mobsoft.mobilsoftwarelab.ui.main;
+
+import com.example.mobsoft.mobilsoftwarelab.ui.Presenter;
+
+/**
+ * Created by Patrik on 2017. 03. 19..
+ */
+
+public class MainPresenter extends Presenter<MainScreen> {
+
+    private static MainPresenter instance = null;
+
+    private MainPresenter() {
+    }
+
+    public static MainPresenter getInstance() {
+        if (instance == null) {
+            instance = new MainPresenter();
+        }
+        return instance;
+    }
+
+    @Override
+    public void attachScreen(MainScreen screen) {
+        super.attachScreen(screen);
+    }
+
+    @Override
+    public void detachScreen() {
+        super.detachScreen();
+    }
+}
---
+++ app/src/main/java/com/example/mobsoft/mobilsoftwarelab/ui/main/MainScreen.java
@@ -0,0 +1,9 @@
+package com.example.mobsoft.mobilsoftwarelab.ui.main;
+
+/**
+ * Created by mobsoft on 2017. 03. 27..
+ */
+
+public interface MainScreen {
+    public void showMessage(String text);
+}
