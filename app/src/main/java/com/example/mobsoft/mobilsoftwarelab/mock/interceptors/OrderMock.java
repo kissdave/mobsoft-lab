cf4251beaf009e2d5385e941d2f66fff00ba6163
kissdave <kiss.david@simonyi.bme.hu>
2017-05-07 17:17:30 +0200

    Networking Apis implemented.

--- app/src/main/java/com/example/mobsoft/mobilsoftwarelab/mock/interceptors/OrderMock.java
+++ app/src/main/java/com/example/mobsoft/mobilsoftwarelab/mock/interceptors/OrderMock.java
@@ -1,5 +1,12 @@
 package com.example.mobsoft.mobilsoftwarelab.mock.interceptors;
 
+import android.net.Uri;
+
+import com.example.mobsoft.mobilsoftwarelab.network.NetworkConfig;
+import com.example.mobsoft.mobilsoftwarelab.repository.MemoryRepository;
+import com.example.mobsoft.mobilsoftwarelab.utils.GsonHelper;
+
+import okhttp3.Headers;
 import okhttp3.Request;
 import okhttp3.Response;
 
@@ -11,9 +18,24 @@ import static com.example.mobsoft.mobilsoftwarelab.mock.interceptors.MockHelper.
 
 public class OrderMock {
     public static Response process(Request request) {
+        Uri uri = Uri.parse(request.url().toString());
 
-        // TODO
+        String responseString;
+        int responseCode;
+        Headers headers = request.headers();
 
+        if (uri.getPath().equals(NetworkConfig.ENDPOINT_PREFIX + "Orders") && request.method().equals("POST")) {
+            responseString = "";
+            responseCode = 200;
+        } else if (uri.getPath().equals(NetworkConfig.ENDPOINT_PREFIX + "Orders") && request.method().equals("Get")) {
+            MemoryRepository memoryRepository = new MemoryRepository();
+            memoryRepository.open(null);
+            responseString = GsonHelper.getGson().toJson(memoryRepository.getOrders());
+            responseCode = 200;
+        } else {
+            responseString = "ERROR";
+            responseCode = 503;
+        }
         return makeResponse(request, headers, responseCode, responseString);
     }
 }
--- app/src/main/java/com/example/mobsoft/mobilsoftwarelab/mock/interceptors/ProductMock.java
+++ app/src/main/java/com/example/mobsoft/mobilsoftwarelab/mock/interceptors/ProductMock.java
@@ -1,5 +1,12 @@
 package com.example.mobsoft.mobilsoftwarelab.mock.interceptors;
 
+import android.net.Uri;
+
+import com.example.mobsoft.mobilsoftwarelab.network.NetworkConfig;
+import com.example.mobsoft.mobilsoftwarelab.repository.MemoryRepository;
+import com.example.mobsoft.mobilsoftwarelab.utils.GsonHelper;
+
+import okhttp3.Headers;
 import okhttp3.Request;
 import okhttp3.Response;
 
@@ -11,6 +18,25 @@ import static com.example.mobsoft.mobilsoftwarelab.mock.interceptors.MockHelper.
 
 public class ProductMock {
     public static Response process(Request request) {
+        Uri uri = Uri.parse(request.url().toString());
+
+        String responseString;
+        int responseCode;
+        Headers headers = request.headers();
+
+        if (uri.getPath().equals(NetworkConfig.ENDPOINT_PREFIX + "Products") && request.method().equals("POST")) {
+            responseString = "";
+            responseCode = 200;
+        } else if (uri.getPath().equals(NetworkConfig.ENDPOINT_PREFIX + "Products") && request.method().equals("Get")) {
+            MemoryRepository memoryRepository = new MemoryRepository();
+            memoryRepository.open(null);
+            responseString = GsonHelper.getGson().toJson(memoryRepository.getProducts());
+            responseCode = 200;
+        } else {
+            responseString = "ERROR";
+            responseCode = 503;
+        }
+
         return makeResponse(request, headers, responseCode, responseString);
     }
 }
