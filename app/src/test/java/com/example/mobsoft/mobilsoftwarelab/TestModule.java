6891ff159ae7697d86f952b0b5758d0484b98a68
kissdave <kiss.david@simonyi.bme.hu>
2017-05-14 02:58:15 +0200

    TestModule fixed

--- app/src/test/java/com/example/mobsoft/mobilsoftwarelab/TestModule.java
+++ app/src/test/java/com/example/mobsoft/mobilsoftwarelab/TestModule.java
@@ -3,7 +3,10 @@ package com.example.mobsoft.mobilsoftwarelab;
 import android.content.Context;
 
 import com.example.mobsoft.mobilsoftwarelab.ui.UIModule;
+import com.example.mobsoft.mobilsoftwarelab.ui.cart.CartPresenter;
 import com.example.mobsoft.mobilsoftwarelab.ui.main.MainPresenter;
+import com.example.mobsoft.mobilsoftwarelab.ui.orders.OrdersPresenter;
+import com.example.mobsoft.mobilsoftwarelab.ui.settings.SettingsPresenter;
 import com.example.mobsoft.mobilsoftwarelab.utils.UiExecutor;
 
 import java.util.concurrent.Executor;
@@ -22,8 +25,8 @@ import dagger.Provides;
 public class TestModule {
     private final UIModule uiModule;
 
-    public TestModule(UIModule uiModule) {
-        this.uiModule = uiModule;
+    public TestModule(Context context) {
+        this.uiModule = new UIModule(context);
     }
 
     @Provides
@@ -35,6 +38,15 @@ public class TestModule {
     public MainPresenter provideMainPresenter() {
         return uiModule.provideMainPresenter();
     }
+
+    @Provides
+    public CartPresenter cartPresenter() { return uiModule.cartPresenter(); }
+
+    @Provides
+    public OrdersPresenter ordersPresenter() { return uiModule.ordersPresenter(); }
+
+    @Provides
+    public SettingsPresenter settingsPresenter() { return uiModule.settingsPresenter(); }
 
     @Provides
     @Singleton
